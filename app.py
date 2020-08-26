9f54eb9bddbe38640b71abf17f010a80a5b3b8a3
GeryLaurent <70259459+GeryLaurent@users.noreply.github.com>
2020-08-26 15:18:59 +0200

    added server attribute

--- app.py
+++ app.py
@@ -173,6 +173,8 @@ figR = px.line(dfMain[dfMain['dep'] == "01"], x="jour", y="rea", title="Nombre a
 figDc = px.line(dfMain[dfMain['dep'] == "01"], x="jour", y="dc", title="Nombre de décès cumulé")
 
 # Initialisation de Dash et mise en page
+server = app.server
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div(className="grid-container",
